415128eb12e7d17f61d2ce4d46021d24aeeeac0a
Joel <anote8phone@gmail.com>
2023-03-14 12:25:41 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//// MODULES, API KEY (JL's) ////
+
+const fs = require("node:fs")
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+//// TRANSCRIBE INTO LANGUAGE OF INPUT AUDIO - LOCAL DIR ////
+
+async function transcribeMe() {
+const resp = await openai.createTranscription(
+    fs.createReadStream("/Users/joellim/Desktop/coda_work_mac_2022/openai-project-2/transcribe-translate/sample-audio-6.mp3"),
+    "whisper-1",
+    "",
+    "",
+    "0.2"
+    //positional params from: https://platform.openai.com/docs/api-reference/audio/create
+);
+console.log(resp.data.text, '\n')
+}
+
+transcribeMe()
+
+
+//// TRANSLATE INPUT AUDIO - LOCAL DIR - TO ENGLISH (ONLY) AND TRANSCRIBE IT ////
+
+async function translateMe() {
+const resp = await openai.createTranslation(
+    fs.createReadStream("/Users/joellim/Desktop/coda_work_mac_2022/openai-project-2/transcribe-translate/sample-audio-6.mp3"),
+    "whisper-1",
+    "",
+    "",
+    "0.2"
+   //positional params from: https://platform.openai.com/docs/api-reference/audio/create
+);
+console.log(resp.data.text, '\n')
+}
+
+//translateMe()
+
